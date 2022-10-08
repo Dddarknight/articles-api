c224806bb36ec57cd5afd5b1db6984f652278d39
Dddarknight <9801677@gmail.com>
2022-10-08 19:49:50 +0300

    change server.py

--- api_server/server.py
+++ api_server/server.py
@@ -8,6 +8,10 @@ from api_server.users import models as users_models
 from api_server.database import engine
 
 
+users_models.Base.metadata.create_all(bind=engine)
+articles_models.Base.metadata.create_all(bind=engine)
+
+
 app = FastAPI()
 
 
@@ -20,12 +24,11 @@ async def startup():
 async def shutdown():
     await database.disconnect()
 
+
 app.include_router(users.router)
 app.include_router(articles.router)
 app.include_router(events.router)
 
-users_models.Base.metadata.create_all(bind=engine)
-articles_models.Base.metadata.create_all(bind=engine)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8075)
